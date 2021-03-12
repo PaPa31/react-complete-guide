0ea92acd3335320c8a917db6c0feb3ec3f76d464
papa <visidoor@yandex.ru>
2021-03-12 19:23:37 +0500

    3.12 Working with Props
    	modified:   src/App.js
    	modified:   src/Person/Person.js

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
-const person = () => {
+// class Person extends React.Component {
+//   render() {
+//     return (
+//       <p>
+//         I'm {this.props.name} and I am {this.props.age} years old!
+//       </p>
+//     )
+//   }
+// }
+const person = (props) => {
   return (
-    <p>I'm a person and I am {Math.floor(Math.random() * 30)} years old!</p>
+    <p>
+      I'm {props.name} and I am {props.age} years old!
+    </p>
   )
 }
 
