86a0cad9c6c48503b625814241f7f996c6bf903d
papa <visidoor@yandex.ru>
2021-03-17 02:51:40 +0500

    3.26.10 Add style
    	modified:   src/UserInput/UserInput.js
    	new file:   src/UserOutput/UserOutput.css
    	modified:   src/UserOutput/UserOutput.js

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
+const style = {
+  border: '2px solid red',
+  padding: '6px',
+  margin: '0 auto',
+  display: 'block',
+  textAlign: 'center',
+}
 const userInput = (props) => {
-  return <input type="text" onChange={props.changed} value={props.name}></input>
+  return (
+    <input
+      style={style}
+      type="text"
+      onChange={props.changed}
+      value={props.name}
+    ></input>
+  )
 }
 
 export default userInput
