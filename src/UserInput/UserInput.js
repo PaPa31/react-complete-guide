833c3edd1a9625fbffbd1b0c8058c3750c655f1f
papa <visidoor@yandex.ru>
2021-03-19 22:03:55 +0500

    3.10 Add styling
    	modified:   src/UserInput/UserInput.js
    	new file:   src/UserOutput/UserOutput.css
    	modified:   src/UserOutput/UserOutput.js

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,7 +1,20 @@
 import React from 'react'
-
+const style = {
+  border: '1px solid red',
+  padding: '6px',
+  textAlign: 'center',
+  display: 'block',
+  margin: '0 auto',
+}
 const userInput = (props) => {
-  return <input type="text" onChange={props.changed} value={props.name}></input>
+  return (
+    <input
+      style={style}
+      type="text"
+      onChange={props.changed}
+      value={props.name}
+    ></input>
+  )
 }
 
 export default userInput
--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './UserOutput.css'
 
 const userOutput = (props) => {
   return (
-    <div>
+    <div className="UserOutput">
       <p>
         I'm {props.name} and I am {props.age} years old!
       </p>
