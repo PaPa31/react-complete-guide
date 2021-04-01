730d4fa5bcce1a7d19910acb883406a0e5cfaa3e
papa <visidoor@yandex.ru>
2021-04-02 00:43:02 +0500

    4.4 Create another component
    	new file:   src/CharComponent.js

---
+++ src/CharComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const charComponent = () => {
+  const style = {
+    display: "inline-block",
+    padding: "16px",
+    textAlign: "center",
+    margin: "16px",
+    border: "1px solid #ccc",
+  };
+  return <div style={style}></div>;
+};
+
+export default charComponent;
