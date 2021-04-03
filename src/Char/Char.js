3087e77ea64d18a8072511c08c512ce6f6939e42
papa <visidoor@yandex.ru>
2021-04-03 10:30:21 +0500

    4.4 Create char component
    	new file:   src/Char/Char.js

---
+++ src/Char/Char.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const char = (props) => {
+  const style = {
+    display: "inline-block",
+    padding: "16px",
+    textAlign: "center",
+    margin: "16px",
+    border: "1px solid #ccc",
+    cursor: "pointer",
+  };
+  return <div style={style}></div>;
+};
+
+export default char;
