ae04ae693d0dedcedd54bc8ed4b1d5f8d82a9437
papa <visidoor@yandex.ru>
2021-04-03 12:16:15 +0500

    4.3 Add output text
    	modified:   src/Validation/Validation.js

--- src/Validation/Validation.js
+++ src/Validation/Validation.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 const validation = (props) => {
+  let validationMessage = "Text too short";
+  if (props.textLength > 5) validationMessage = "Text long enough";
   return (
     <div>
-      <p>{props.textLength}</p>
+      <p>{validationMessage}</p>
     </div>
   );
 };
