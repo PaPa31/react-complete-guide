2bffd86a2549d0c71c2952b01f59bc53a255a115
papa <visidoor@yandex.ru>
2021-04-06 09:09:58 +0500

    6.5 Using Error Boundaries
    	modified:   src/App.js
    	new file:   src/ErrorBoundary/ErrorBoundary.js
    	modified:   src/Person/Person.js
    
    	src/App.js

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -26,6 +26,10 @@ const StyleDiv = styled.div`
 `;
 
 const person = (props) => {
+  const rnd = Math.random();
+  if (rnd > 0.7) {
+    throw new Error("Something went wrong");
+  }
   return (
     // <div className="Person" style={style}>
     <StyleDiv>
