e0e3cceefc9d64f4a10bae1396335a944053d9a0
papa <visidoor@yandex.ru>
2021-04-07 15:06:17 +0500

    7.7 Component Creation Lifecycle in Action
    	modified:   src/components/Persons/Person/Person.js
    	modified:   src/components/Persons/Persons.js
    	modified:   src/containers/App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,10 @@ import Cockpit from "../components/Cockpit/Cockpit";
 import Persons from "../components/Persons/Persons";
 import classes from "./App.css";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("[App.js] constructor");
+  }
   state = {
     persons: [
       { id: "20210330005200", name: "Max", age: 28 },
@@ -13,6 +17,17 @@ class App extends Component {
     ],
   };
 
+  static getDerivedStateFromProps(props, state) {
+    console.log("[App.js] getDerivedStateFromProps", props);
+    return state;
+  }
+
+  componentWillMount() {
+    console.log("[App.js] componentWillMount");
+  }
+  componentDidMount() {
+    console.log("[App.js] componentDidMount");
+  }
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex((p) => {
       return p.id === id;
@@ -47,6 +62,7 @@ class App extends Component {
   };
 
   render() {
+    console.log("[App.js] render");
     let persons = null;
 
     if (this.state.showPersons) {
