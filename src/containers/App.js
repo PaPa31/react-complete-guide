9de23606ec7fc2f9f06934b81e166457c8b2fd1e
papa <visidoor@yandex.ru>
2021-04-08 20:24:23 +0500

    7.25 Setting State Correctly
    	modified:   src/containers/App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,6 +18,7 @@ class App extends Component {
       { id: "20210330005209", name: "Stephanie", age: 26 },
     ],
     showCockpit: true,
+    changeCounter: 0,
   };
 
   static getDerivedStateFromProps(props, state) {
@@ -55,8 +56,11 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({
-      persons: persons,
+    this.setState((prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1,
+      };
     });
   };
 
