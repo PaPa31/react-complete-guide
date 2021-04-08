98585030b271f9ecb779c554d1c3266d75cc4c79
papa <visidoor@yandex.ru>
2021-04-08 19:13:01 +0500

    7.24 Passing Unknown Props
    	modified:   src/components/Persons/Person/Person.js
    	modified:   src/hoc/withClass.js

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from "react";
-
+import withClass from "../../../hoc/withClass";
+import classes from "./Person.css";
 class Person extends Component {
   render() {
     console.log("[Person.js] rendering...");
@@ -20,4 +21,4 @@ class Person extends Component {
   }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
--- src/hoc/withClass.js
+++ src/hoc/withClass.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 const withClass = (WrappedComponent, className) => {
-  return (peops) => (
+  return (props) => (
     <div className={className}>
-      <WrappedComponent />
+      <WrappedComponent {...props} />
     </div>
   );
 };
