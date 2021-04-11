d81622a0ed74ddb2676a820e79acc6d1a6a560e0
papa <visidoor@yandex.ru>
2021-04-11 10:23:28 +0500

    refactor(BurgerBuilder): move folder from src/ to
    containers/
    
    	modified:   src/App.js
    	renamed:    src/BurgerBuilder/BurgerBuilder.js -> src/containers/BurgerBuilder/BurgerBuilder.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import BurgerBuilder from "./BurgerBuilder/BurgerBuilder";
 import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 class App extends Component {
   render() {
