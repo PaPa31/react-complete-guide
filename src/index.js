061e367cfe8a39c189c73dcc5bab7d15bbeb0a47
papa <visidoor@yandex.ru>
2021-04-13 18:52:08 +0500

    9.15 Setting Default Global Configuration for Axios
    	modified:   src/components/FullPost/FullPost.js
    	modified:   src/components/NewPost/NewPost.js
    	modified:   src/containers/Blog/Blog.js
    	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from "./App";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
+axios.defaults.headers["Authorization"] = "AUTH TOKEN";
+axios.defaults.headers.post["Content-Type"] = "application/json";
+
 axios.interceptors.request.use(
   (request) => {
     console.log(request);
