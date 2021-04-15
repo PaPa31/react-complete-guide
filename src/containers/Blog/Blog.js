799abf16d19c1d3e728360c62a504b5a8320fc12
papa <visidoor@yandex.ru>
2021-04-15 10:40:51 +0500

    11.9 Switching Between Pages
    	modified:   src/containers/Blog/Blog.js

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,6 +2,7 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import "./Blog.css";
+import NewPost from "./NewPost/NewPost";
 import Posts from "./Posts/Posts";
 
 class Blog extends Component {
@@ -23,6 +24,7 @@ class Blog extends Component {
         {/* <Route path="/" exact render={() => <h1>Home</h1>} />
         <Route path="/" render={() => <h1>Home2</h1>} /> */}
         <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
       </div>
     );
   }
