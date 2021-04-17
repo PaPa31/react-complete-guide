303f5de6baa5bd60fc134bd29d12dfd0837c6a9c
papa <visidoor@yandex.ru>
2021-04-17 21:48:25 +0500

    setup

--- src/index.js
+++ src/index.js
@@ -1,37 +1,8 @@
-import axios from "axios";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import "./index.css";
+import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-
-axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
-axios.defaults.headers["Authorization"] = "AUTH TOKEN";
-axios.defaults.headers.post["Content-Type"] = "application/json";
-
-axios.interceptors.request.use(
-  (request) => {
-    console.log(request);
-    // Edit request config
-    return request;
-  },
-  (error) => {
-    console.log(error);
-    return Promise.reject(error);
-  }
-);
-
-axios.interceptors.response.use(
-  (response) => {
-    console.log(response);
-    // Edit response config
-    return response;
-  },
-  (error) => {
-    console.log(error);
-    return Promise.reject(error);
-  }
-);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
