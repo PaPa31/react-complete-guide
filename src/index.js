a892cfeca213591fa444074eb941dd14507d6b0e
papa <visidoor@yandex.ru>
2021-04-17 18:55:29 +0500

    11.30.2 Routing React Suspense start

--- src/index.js
+++ src/index.js
@@ -1,37 +1,8 @@
-import axios from "axios";
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
-import registerServiceWorker from "./registerServiceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
-axios.defaults.headers["Authorization"] = "AUTH TOKEN";
-axios.defaults.headers.post["Content-Type"] = "application/json";
+import './index.css';
+import App from './App';
 
-axios.interceptors.request.use(
-  (request) => {
-    console.log(request);
-    // Edit request config
-    return request;
-  },
-  (error) => {
-    console.log(error);
-    return Promise.reject(error);
-  }
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
-axios.interceptors.response.use(
-  (response) => {
-    console.log(response);
-    // Edit response config
-    return response;
-  },
-  (error) => {
-    console.log(error);
-    return Promise.reject(error);
-  }
-);
-
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
