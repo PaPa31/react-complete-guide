e4a72ec2c0404b5958949fd1d2d14b2eeb26f5da
papa <visidoor@yandex.ru>
2021-04-18 23:23:18 +0500

    5.2 Pass course.title (as query params)

--- src/containers/Courses/Courses.js
+++ src/containers/Courses/Courses.js
@@ -19,7 +19,10 @@ class Courses extends Component {
           {this.state.courses.map((course) => {
             return (
               <Link
-                to={this.props.match.url + "/" + course.id + "/" + course.title}
+                to={{
+                  pathname: this.props.match.url + "/" + course.id,
+                  search: "?title=" + course.title,
+                }}
                 key={course.id}
               >
                 <article className="Course">{course.title}</article>
