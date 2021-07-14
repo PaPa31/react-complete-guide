10842349379d45a617ef8bceef322e9dd2626a8b
papa31 <visidoor@yandex.ru>
2021-07-14 22:37:59 +0500

    17.2 Installing the Redux Devtools

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 import reducer from "./store/reducer";
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const app = (
   <Provider store={store}>
