26bcdd0b0c37c6e05fdcbd4a246c251c673c0593
papa31 <visidoor@yandex.ru>
2021-07-14 11:02:45 +0500

    16.5 Introducing Action Creators

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import CounterControl from "../../components/CounterControl/CounterControl";
 import CounterOutput from "../../components/CounterOutput/CounterOutput";
-import * as actionTypes from "../../store/actions";
+import { increment } from "../../store/actions/actions";
 
 class Counter extends Component {
   render() {
@@ -50,7 +50,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onIncrementCounter: () => dispatch({ type: actionTypes.INCREMENT }),
+    onIncrementCounter: () => dispatch(increment()),
     onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
     onAddCounter: () => dispatch({ type: actionTypes.ADD, value: 5 }),
     onSubtractCounter: () => dispatch({ type: actionTypes.SUBTRACT, value: 5 }),
